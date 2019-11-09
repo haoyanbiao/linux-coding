400a08761a7cdbd380cc29a59fd197709e38b6da
haoyanbiao <haoyanbiao@126.com>
2019-11-09 02:23:33 -0800

    add main.c

---
+++ main.c
@@ -0,0 +1,15 @@
+
+    #include <stdio.h>
+
+    #include <stdint.h>
+
+    int main(int argc, const char *argv[])
+
+    {
+
+        
+            printf("hello world\n");
+
+        return 0;
+
+    }
